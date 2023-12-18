bfb8ac89b726c9f47f5a35c99472defc883cba5c
Thijs Moerland <thijs@fivex.com>
2023-12-18 13:05:01 +0100

    vue init test home, test about page routing working

---
+++ Webdev2FrontEnd/Webdev2/src/components/About.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>About</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AboutPage",
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Webdev2FrontEnd/Webdev2/src/components/StockList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Stock List</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "StockList",
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
