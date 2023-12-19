b47cf20490d944eaa111eb0ad7540050742eec03
Thijs Moerland <thijs@fivex.com>
2023-12-19 12:30:40 +0100

    parent / child things working

---
+++ Webdev2FrontEnd/Webdev2/src/components/StockItem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="col-md-4 mt-4">
+        <div class="card">
+            <div class="card-body">
+                <td>{{ name }}</td>
+                <span
+                    :class="{
+                        down: price < previousPrice,
+                        up: price > previousPrice,
+                    }"
+                >
+                    {{ currency }} {{ price.toFixed(4) }}
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "StockItem",
+    props: {
+        name: String,
+        currency: String,
+        price: Number,
+        previousPrice: Number,
+    }
+}
+</script>
+
+<style scoped>
+.up {
+    color: green;
+}
+.down {
+    color: red;
+}
+</style>
--- Webdev2FrontEnd/Webdev2/src/components/StockList.vue
+++ Webdev2FrontEnd/Webdev2/src/components/StockList.vue
@@ -1,26 +1,47 @@
 <template>
-    <div>
-        <h1>Stock List</h1>
+    <h1 class="pt-5">Stock list</h1>
+    <div class="row">
+        <stock-item v-for="stock in stocks" :key="stock.name" :name="stock.name" :currency="stock.currency" :price="stock.price" :previousPrice="stock.previousPrice"></stock-item>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "StockList",
-        data() {
-            return {
+import StockItem from './StockItem.vue';
 
-            }
+export default {
+    name: "StockList",
+    components: {
+        StockItem,
+    },
+    data() {
+        return {
+            stocks: [
+                { name: "BMW", price: 61.05, previousPrice: 0, currency: "€" },
+                { name: "Kawasaki", price: 146.49, previousPrice: 0, currency: "$" },
+                { name: "AMD", price: 76.5, previousPrice: 0, currency: "$" },
+                { name: "Gazprom", price: 4.583, previousPrice: 0, currency: "$" },
+            ],
+        };
+    },
+    methods: {
+        updatePrices() {
+            this.stocks.forEach((stock) => {
+                stock.previousPrice = stock.price;
+                stock.price += (Math.random() - 0.5) * 2;
+                if (stock.price < 0) {
+                    stock.price = 0;
+                }
+            });
         },
-        methods: {
-
-        },
-        mounted() {
-
-        }
-    }
+    },  
+    mounted() {
+        setInterval(() => {
+            this.updatePrices();
+        }, 5000);
+    },
+};
 </script>
 
 <style scoped>
-
 </style>
+
