f59d6629bc3381064074abec394e1193f4376c9b
Thijs Moerland <thijs@fivex.com>
2024-01-15 17:58:52 +0100

    Post new motorcycles

--- Webdev2FrontEnd/Webdev2/src/components/admin/createMotorcycle.vue
+++ Webdev2FrontEnd/Webdev2/src/components/admin/createMotorcycle.vue
@@ -5,22 +5,45 @@
             <div class="card-body">
                 <form @submit.prevent="addMotorcycle">
                     <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" id="name" v-model="motorcycle.name" class="form-control" required>
+                        <label for="brand">Brand:</label>
+                        <input type="text" id="brand" class="form-control" v-model="motorcycle.brand" required>
                     </div>
                     <div class="form-group">
-                        <div class="form-group">
-                            <label for="currency">Currency</label>
-                            <select id="currency" v-model="motorcycle.currency" class="form-control" required>
-                                <option value="€" selected>Euro (€)</option>
-                                <option value="$">Dollar ($)</option>
-                            </select>
+                        <label for="type">Type:</label>
+                        <input type="text" id="type" class="form-control" v-model="motorcycle.type" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="constructionYear">Construction Year:</label>
+                        <input type="date" id="constructionYear" class="form-control" v-model="motorcycle.constructionYear" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="power">Power:</label>
+                        <input type="number" id="power" class="form-control" v-model="motorcycle.power" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="power">Price:</label>
+                        <input type="number" id="price" class="form-control" v-model="motorcycle.price" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="mass">Mass:</label>
+                        <input type="number" id="mass" class="form-control" v-model="motorcycle.mass" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="engine">Engine:</label>
+                        <input type="number" id="engine" class="form-control" v-model="motorcycle.engine" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="accessories">Accessories:</label>
+                        <input type="text" id="accessories" class="form-control" v-model="motorcycle.accessories" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="imgUrl">Image URL:</label>
+                        <input type="text" id="imgUrl" class="form-control" v-model="motorcycle.img_url" required>
+                    </div>
+                    <div class="form-group">
+                            <input type="checkbox" id="sold" name="sold"  v-model="motorcycle.sold">
+                            <label for="sold">&nbsp;Sold</label>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="price">Price</label>
-                        <input type="number" id="price" v-model="motorcycle.price" class="form-control" required>
-                    </div>
                     <button type="submit" class="btn btn-primary">Add Motorcycle</button>
                 </form>
             </div>
@@ -29,23 +52,39 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
     name: "CreateMotorcycle",
     data() {
         return {
             motorcycle: {
-                name: "",
-                currency: "",
-                price: null,
-                previousPrice: null,
+                brand: "",
+                type: "",
+                constructionYear: null,
+                power: 0,
+                price: 0,
+                mass: 0,
+                engine: "",
+                accessories: "",
+                img_url: "",
+                sold: false,
             },
         };
     },
     methods: {
         addMotorcycle() {
+            axios
+                .post("http://localhost/motorcycles/", this.motorcycle)
+                .then((result) => {
+                    console.log(result);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
             // Logic to add the motorcycle
             alert("Motorcycle added!");
-            this.$router.push("/"); // Replace "/home" with the actual path to your home component
+            // this.$router.push("/"); // Replace "/home" with the actual path to your home component
         },
     },
     mounted() {},
